0db77dd247bd68c230cb8a30ecae22fc3825ffc3
Jeff Chen <jkchenweb@gmail.com>
2021-01-20 21:30:59 -0800

    HTTPS no keys

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Secure traffic only
+app.all('*', (req, res, next) => {
+    if (req.secure) {
+      return next();
+    } else {
+        console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+        res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+    }
+});
+
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
