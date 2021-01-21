b4d228103c9900328c0a212e45005cbebca07ab2
Jeff Chen <jkchenweb@gmail.com>
2021-01-20 21:54:50 -0800

    File Upload

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 const passport = require('passport');
 const config = require('./config');
+const uploadRouter = require('./routes/uploadRouter');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -71,6 +72,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
