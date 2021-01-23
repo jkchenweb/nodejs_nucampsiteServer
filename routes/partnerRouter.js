46ce7a23069c3b525ddf5d7346fea3ea5ff202f1
Jeff Chen <jkchenweb@gmail.com>
2021-01-22 22:09:18 -0800

    CORS

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -2,16 +2,59 @@ const express = require('express');
 const partnerRouter = express.Router();
 const Partner = require('../models/partner');
 const authenticate = require('../authenticate');
-partnerRouter.route('/:partnerId')
+const cors = require('./cors');
 
-.all( (req, res, next) => {
+partnerRouter.route('/')
+
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+
+.all(cors.cors, (req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     //maybe we should check the type of req.params.partnerId
     next();
 })
 
-.get ((req, res, next)=>{
+.get(cors.cors, (req, res, next) => {
+    Partner.find()
+    .then(partners=>{
+        res.json(partners);
+    })
+    .catch(err => next(err));
+})
+
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Partner.create(req.body)
+    .then(partner=>{
+        console.log('Partner Created ', partner);
+        res.json(partner);
+    })
+    .catch(err => next(err));
+})
+
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partners');
+})
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Partner.deleteMany()
+    .then(response => {
+        res.json(response);
+    })
+    .catch(err => next(err));
+});
+
+partnerRouter.route('/:partnerId')
+.all(cors.cors, (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    //maybe we should check the type of req.params.partnerId
+    next();
+})
+
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+
+.get(cors.cors, (req, res, next) => {
     Partner.findById(req.params.partnerId)
     .then(partner=>{
         res.json(partner);
@@ -19,12 +62,12 @@ partnerRouter.route('/:partnerId')
     .catch(err => next(err));
 })
 
-.post(authenticate.verifyUser, (req, res)=>{
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     res.statusCode = 403;
     res.end(`POST operation not supported on /partners/${req.params.partnerId}`)
 })
 
-.put (authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
     Partner.findByIdAndUpdate(req.params.partnerId, {
         $set: req.body
     },{
@@ -36,52 +79,12 @@ partnerRouter.route('/:partnerId')
     .catch(err => next(err));
 })
 
-.delete(authenticate.verifyUser, authenticate.verifyAdmin,  (req, res, next)=>{
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Partner.findByIdAndDelete(req.params.partnerId)
     .then(response=>{
         res.json(response);
     })
     .catch(err => next(err));
 });
-
-partnerRouter.route('/')
-
-.all( (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    //maybe we should check the type of req.params.partnerId
-    next();
-})
-
-.get ((req, res, next)=>{
-    Partner.find()
-    .then(partners=>{
-        res.json(partners);
-    })
-    .catch(err => next(err));
-})
-
-.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next)=>{
-    Partner.create(req.body)
-    .then(partner=>{
-        console.log('Partner Created ', partner);
-        res.json(partner);
-    })
-    .catch(err => next(err));
-})
-
-.put(authenticate.verifyUser, (req,res)=>{
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /partners');
-})
-.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    Partner.deleteMany()
-    .then(response => {
-        res.json(response);
-    })
-    .catch(err => next(err));
-});
-
-
 
 module.exports = partnerRouter;
